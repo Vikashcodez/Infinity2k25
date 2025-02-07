89c2b3b619c4aa8bfd05213e5bdfcf7085124fe4
siddardhakumar2003 <158472582+siddardhakumar2003@users.noreply.github.com>
2025-02-07 11:26:58 +0000

    event

--- infinity/src/Pages/Events.jsx
+++ infinity/src/Pages/Events.jsx
@@ -1,28 +1,30 @@
 import react , { useState } from "react";
 import slides from "../data/slides.json"
 import "./Events.css";
+import  I1 from "../data/1.jpeg";
+import  I2 from "../data/2.jpeg";
+import I3 from "../data/3.jpeg";
+
 import { BsArrowLeftCircleFill ,BsArrowRightCircleFill } from 'react-icons/bs';
 
 const Events = () => {
     const [present,setPresent]=useState(0);
-
-
+    const images=[I1,I2,I3];
     const data=slides.slides;
-    return <div className="container">
+    return <div className="main">
+        <div className="container">
         <BsArrowLeftCircleFill className="arrow arrow-left" onClick={()=>{
             setPresent((present+data.length-1)%data.length);
         }}/>
        {data.map((item,index)=> (
-            <img 
-            src={item}
+            <a href= {`#${index}`} ><img 
+            src={images[index]}
             key={index}
             width='600vw'
-            height='5%'
-            className={(index===present)?"slide":"slide-i"}/>
+            height='100%'
+            className={(index===present)?"slide":"slide-i"}
+            onClick={scrollTo()}/></a>
         ))} 
-        <BsArrowRightCircleFill className="arrow arrow-right" onClick={()=>{
-            setPresent((present+1)%data.length);
-        }}/>
         <span className="indicators">
             {data.map((_,index)=>(
                  <button  className={(index===present)?"indicator":"indicator-i"} onClick={()=>{
@@ -30,6 +32,18 @@ const Events = () => {
                  }} key={index}></button>
             ))}
         </span>
+        <BsArrowRightCircleFill className="arrow arrow-right" onClick={()=>{
+            setPresent((present+1)%data.length);
+        }}/>
+        </div>
+        <div>
+            {data.map((item,index)=>(
+                <div id={`${index}`} className="container-2">
+                    <h1>{item.title}</h1><br/>
+                    <p>{item.details}</p>
+                </div>
+            ))}
+        </div>
     </div>
 }
 
